758170f837427ef09be40d8b130d6fd2fa322347
dered-dev <jlcc170@gmail.com>
2022-05-17 22:32:12 -0500

    ADD class/Class-one

--- js/base.js
+++ js/base.js
@@ -1,48 +1,50 @@
 
 // crear variables
-// let, const, var
-// Palabras reservadas
+// let, const, var son Palabras reservadas
 
 // Constantes
 const koder3 = 'Ernesto' // no  la puedo modificar
+const URL = 'https://www.google.com'
+const PI = 3.1416
 
 // variables
 var koder = 'Jorge' // se pueden modificar
 let koder2 = 'Noemi' // se pueden modificar
 
-const URL = 'https://www.google.com'
-const PI = 3.1416
+
 // scope
 // let para variables
 // const para constantes  
 
-// Concatenar variables
-// operador typeof 
-// let valor1 = 5
-// let valor2 = 3
-// console.log(valor1 + valor2)
 
+// operador typeof 
+let valor1 = 5
+let valor2 = 3
+console.log(valor1 + valor2)
+console.log(typeof valor1 )
+
+// Concatenar variables
 // 'string' + variable
 let koderExample = 'Jorge'
 let nombreConcatenado = 'Hola ' + koderExample
 // console.log(nombreConcatenado)
-// template strings + string literals
+// template literals
 // placeholders
 // `string  ${variable} `
 
 const age = 30
-// console.log(`Hola ${koder}, tienes  ${age} años`)
+console.log(`Hola ${koder}, tienes  ${age} años`)
 
 // Pedir valores al usuario
 // prompt(mensaje, valorDelMensajePorDefecto)
 // let nombreKoder = prompt('Dame tu nombre', 'Jorge')
 
-// if( nombreKoder !== null ) {
-//     console.log(nombreKoder)
-//     // ...
-// } else {
-//     console.log('Nombre vacio')
-// }
+if( nombreKoder !== null ) {
+    console.log(nombreKoder)
+    // ...
+} else {
+    console.log('Nombre vacio')
+}
 
 // Convertir strings a numeros
 // let ageKoder = prompt('Dame tu edad', 30)
@@ -53,6 +55,7 @@ const age = 30
 // convertir ºC to ºF
 // celsius * 9/5 + 32
 // calcular los ºF
+
 let celsius = prompt('Temperatura actual', 23)
 if( celsius === null ) {
     console.log('Temperatura incorrecta')    
