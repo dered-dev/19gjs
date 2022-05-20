a0f0ba105dcce21975e225a5895ba52132ac2297
dered-dev <jlcc170@gmail.com>
2022-05-19 22:45:09 -0500

    ADD homework

--- js/ciclos.js
+++ js/ciclos.js
@@ -25,11 +25,10 @@ for(let i = 1; i <= 10; i++){
 // 'hola koders'
 // '01234567891'
 // '12345678901'
-
+// imprimir cada palabra del string
 let str = 'Hola Koder'
 let fin = str.length
-console.log(fin)
-// console.log(str[0])
+
 
 // for(let i = 0; i < fin; i++){
 //     console.log(str[i])
@@ -39,13 +38,39 @@ console.log(fin)
 // Imprimir en consola cada una de las vocales
 // ciclos y condicionales
 
+
 for(let i = 0; i < fin; i++){
     let caracter = str[i]
-    if(caracter == 'a' || caracter == 'e' || caracter == 'i' || caracter == 'o' || caracter == 'u'){
+    // console.log(caracter)
+    if(
+        caracter == 'a' || 
+        caracter == 'e' || 
+        caracter == 'i' || 
+        caracter == 'o' || 
+        caracter == 'u'
+    ){
         console.log(caracter)
     }
 } 
 
+// "Hola Koder"
+// Imprimir en consola cada una de las consonantes
+// ciclos y condicionales
+
+for(let i = 0; i < fin; i++){
+    let caracter = str[i]
+    // console.log(caracter)
+    if(
+        caracter !== 'a' && 
+        caracter !== 'e' && 
+        caracter !== 'i' && 
+        caracter !== 'o' && 
+        caracter !== 'u' &&
+        caracter !== ' ' 
+    ){
+        console.log(caracter)
+    }
+} 
 
 
 
---
+++ js/homework.js
@@ -0,0 +1,49 @@
+// Ejercicio 1
+// investigar while, y do while
+
+
+// Utilizar Condicionales, Ciclo for, Metodos de string
+
+/**
+ * Ejercicio 2
+ * Pedir al usuario usuario un numero entre 1 y 10
+ * Imprimir la tabla de multiplicar de ese número
+ * p.ej. 3
+ * 3 x 1 = 3
+ * 3 x 2 = 6
+ * ....
+ * 3 x 10 = 30
+ */
+
+
+
+/**
+ * Ejercicio 3
+ * Pedir al usuario una oracion
+ * Imprimir un string con todas las vocales
+ * Imprimir un string con todas las consonantes
+ * p.ej. "Hola mundo"
+ * Consonantes -> hlmnd
+ * Vocales -> oauo
+ */
+
+
+/**
+ * 
+ * Ejercicio 4
+ * Pedir el usuario un numero entre 10 y 100
+ * Imprimir todos los numeros pares que existen entre 1 y ese numero
+ * p.ej. 12 -> 2,4,6,8,10,12
+ * 
+ */
+
+
+
+/**
+ * 
+ * Ejercicio 5. Opcional
+ * Pedir el usuario una palabra
+ * Invertir esa palabra e imprimirla en consola
+ * p.ej. 'Hola' -> 'aloh'
+ *
+ */
