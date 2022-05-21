4296d82b059255819c1ceb493db1ff9f2d087137
dered-dev <jlcc170@gmail.com>
2022-05-20 22:40:40 -0500

    ADD homework

--- js/ciclos.js
+++ js/ciclos.js
@@ -7,70 +7,98 @@
         // sentences
     }
 
-    while(){}
+    while(expresion){
         // sentences
+
+    }
+
     do{
         // sentences
-    }while()
+    } while(expresion)
 */
 
 
 for(let i = 1; i <= 10; i++){
-    // sentencias
-    // console.log('5 x '+ i)
     console.log(` 5 x ${i} = ${5*i} `)
-} 
+}
 
 
-// 'hola koders'
-// '01234567891'
-// '12345678901'
-// imprimir cada palabra del string
+let counter = 1
+while(counter <= 10) {
+    console.log(` 5 x ${counter} = ${ 5 * counter } `)
+    counter++
+}
+
+
+do {
+    console.log(counter)
+    // console.log(` 5 x ${counter} = ${ 5 * counter } `)
+    counter++
+} while(counter <= 10)
+
+
+
+
+/**
+ * 
+ * Imprimir cada caracter de un string
+ * 
+ */ 
+
 let str = 'Hola Koder'
-let fin = str.length
+let fin = str.length - 1
 
-
-// for(let i = 0; i < fin; i++){
-//     console.log(str[i])
-// } 
-
-// "Hola Koder"
-// Imprimir en consola cada una de las vocales
-// ciclos y condicionales
-
-
+// Solucion for
 for(let i = 0; i < fin; i++){
-    let caracter = str[i]
-    // console.log(caracter)
-    if(
-        caracter == 'a' || 
-        caracter == 'e' || 
-        caracter == 'i' || 
-        caracter == 'o' || 
-        caracter == 'u'
-    ){
-        console.log(caracter)
-    }
+    console.log(str[i])
 } 
 
-// "Hola Koder"
-// Imprimir en consola cada una de las consonantes
-// ciclos y condicionales
+// solucion while
+let counter2 = 0
+while(counter2 <= fin) {
+    console.log( str[counter2] )
+    counter2++
+}
 
-for(let i = 0; i < fin; i++){
-    let caracter = str[i]
-    // console.log(caracter)
-    if(
-        caracter !== 'a' && 
-        caracter !== 'e' && 
-        caracter !== 'i' && 
-        caracter !== 'o' && 
-        caracter !== 'u' &&
-        caracter !== ' ' 
-    ){
-        console.log(caracter)
-    }
-} 
+counter2 = 0
+do {
+    // console.log(counter2)
+    let letra = str[counter2]
+    console.log( letra )
+    counter2++
+} while(counter2 <= fin)
+
+
+/**
+ * 'Hola Koder'
+ * -> 'redok aloh'
+ * Imprimir el string invertido
+ * 
+ * 
+ */ 
+
+ let counterReverse = fin
+ let ReversedStr = ''
+ while(counterReverse >= 0) {
+     console.log( str[counterReverse] )
+     ReversedStr += str[counterReverse]
+     counterReverse--
+ }
+
+ console.log(ReversedStr)
+
+/**
+ * 'oaoe'
+ * Imprimir en consola cada una de las vocales
+ * 
+ */ 
+
+
+/**
+ * 'HlKdr'
+ * Imprimir en consola cada una de las consonantes
+ * 
+ */ 
 
 
 
---
+++ js/homework.js
@@ -0,0 +1,55 @@
+// Ejercicios
+
+/**
+ * Ejercicio 1
+ * Escribir un programa de arroje la suma de los multiplos de 3, 5 o 7 que hay entre 1 y 100
+ * Resolver con For, While y do While
+ */
+
+
+
+/**
+ * Ejercicio 2
+ * Pedir el usuario 1 numero entre 1 y 100
+ * sumar todos los numeros entre el 1 y este numero
+ * Mandar un alert con el  total
+ * Resolver con For, While y do While
+ * p.ej. 5
+ * ->  15
+ */
+
+
+/**
+ * Ejercicio 3
+ * pedir una oracion al usuario
+ * Contar las letras "a" y "e" y espacios,
+ * Resolver con For, While y do While
+ * 
+ * p.ej. 'hola kodErs'
+ * ->  A -> 1 
+ * ->  E -> 1
+ * ->  Espacios -> 1 
+ */
+
+
+/**
+ * Ejercicio 4
+ * Estudiar acerca del tema Funciones
+ * ¿Qué son?
+ * Parametros y argumentos de una funcion
+ * Parametros de una funcion
+ * Parametros opcionales
+ * Parametros por defecto (default)
+ * 
+ * Referencias
+ * https://developer.mozilla.org/es/docs/Web/JavaScript/Guide/Functions
+ * https://www.programiz.com/javascript/function
+ * 
+ */
+
+
+
+
+
+
+
