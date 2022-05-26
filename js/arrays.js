f3c128488eb72307f45b7ea2f59c1066339c9c4d
dered-dev <jlcc170@gmail.com>
2022-05-25 20:33:12 -0500

    ADD foreach exp

---
+++ js/arrays.js
@@ -0,0 +1,51 @@
+
+// Metodos de array
+// .forEach()
+
+// .map()
+// .filter()
+// .reduce()
+// .find()
+
+
+
+// let arr = [10, 20, 30, 40]
+
+// for(i = 0; i < arr.length; i++){
+//     console.log(arr[i])
+// }
+
+// arr.forEach((valorDeCadaIteracion, indiceDelElemento, arrayOriginal) => {})
+let arr = [10, 20, 30, 40, 50, 60, 70]
+
+arr.forEach( (currentValue, index, arr) => {
+    console.log(currentValue, arr)
+} )
+
+// arr.forEach( (currentValue) => {
+//     console.log(currentValue)
+// } )
+
+// arr.forEach( currentValue => {
+//     console.log(currentValue)
+// } )
+
+// arr.forEach( currentValue => console.log(currentValue) )
+
+let arrCities = ['méxicO','RIo', 'Los AngelEs']
+
+/**
+ * Escribir una funcion
+ * Reciba como parametro Un array de ciudades
+ * Retornar las ciudades capitalizadas
+ * capitalize(['méxicO','RIo', 'Los AngelEs'])
+ * -> ['México','Rio', 'Los Angeles']
+ * 
+ */
+const capitalize = (arr) => {
+    
+}
+
+
+
+
