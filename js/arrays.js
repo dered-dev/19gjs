b12e09a557f44a14b01504d99abe8a183fe47002
dered-dev <jlcc170@gmail.com>
2022-05-25 23:07:34 -0500

    ADd homework

--- js/arrays.js
+++ js/arrays.js
@@ -1,7 +1,6 @@
 
 // Metodos de array
 // .forEach()
-
 // .map()
 // .filter()
 // .reduce()
@@ -18,9 +17,9 @@
 // arr.forEach((valorDeCadaIteracion, indiceDelElemento, arrayOriginal) => {})
 let arr = [10, 20, 30, 40, 50, 60, 70]
 
-arr.forEach( (currentValue, index, arr) => {
-    console.log(currentValue, arr)
-} )
+// arr.forEach( (currentValue, index, arr) => {
+//     console.log(currentValue, arr)
+// } )
 
 // arr.forEach( (currentValue) => {
 //     console.log(currentValue)
@@ -43,8 +42,80 @@ let arrCities = ['méxicO','RIo', 'Los AngelEs']
  * 
  */
 const capitalize = (arr) => {
-    
+    let capCities = []
+    arr.forEach( (city) => {
+        let cityCapitalized = `${city[0].toUpperCase()}${city.slice(1).toLowerCase() }`
+        capCities.push(cityCapitalized)
+    })
+    return capCities
 }
+
+const capitalizeMap = (arr) => {
+    let capCities = arr.map( (city) => {
+        return `${city[0].toUpperCase()}${city.slice(1).toLowerCase() }`
+    })
+    return capCities
+}
+
+const capitalizeMapReduced = (arr) => {
+    // return ['México','Rio', 'Los Angeles']
+    // ['México','Rio', 'Los Angeles'] 
+    return arr.map( (city) => {
+        return `${city[0].toUpperCase()}${city.slice(1).toLowerCase() }`
+    })
+}
+
+
+
+
+/**
+ * .map()
+ * [1,2,3,4]
+ * -> [2,4,6,8]
+ */
+
+let mapArr = [1,2,3,4]
+// let interMap = [17, 19, 21, 23]
+let doubleArr = mapArr.map( (cv) => {
+    let transformado = cv * 2
+    transformado += 5
+    transformado += 10
+
+    return transformado
+})
+
+
+let arrStr = ['hola','mundo']
+
+/**
+ * Funcion
+ * getFirstLast(['hola','mundo'])
+ * output -> ['ha','mo']
+ * .map()
+ */
+
+const getFirstLast = (arr) => {
+    let arraySliced = arr.map((item) => {
+        return `${ item.slice(0) }${ item.slice(-1)}`
+    })
+    return arraySliced
+}
+
+const getFirstLastReduced = (arr) => {
+    return arr.map((item) => {
+        return `${ item.slice(0) }${ item.slice(-1)}`
+    })
+}
+
+const getFirstLastMostReduced = (arr) => {
+    return arr.map( item => `${ item.slice(0) }${ item.slice(-1)}` )
+}
+
+const getReduced = arr => arr.map( item => `${ item.slice(0) }${ item.slice(-1)}` )
+
+
+
+
 
 
 
---
+++ js/homework.js
@@ -0,0 +1,36 @@
+
+/**
+ * Funcion que reciba una palabra
+ * Retorne la palabra invertida
+ * reverseStr('hola mundo')
+ * -> 'odnum aloh'
+ * Usar solo metodos de str y array
+ */
+
+
+/**
+ * Funcion que reciba un array de numeros
+ * y devuelva el promedio
+ * -> getAverage( [10, 8, 9, 7] )
+ * -> 8.5
+ */
+
+
+/**
+ * Funcion que reciba un array de strings
+ * y devuelva un array indicando si son palindromos o no
+ * -> arePalindrome( ['oso','juan', 'seres'] )
+ * -> [true, false, true]
+ */
+
+
+/**
+ * Estudiar los métodos de array
+ * .filter()
+ * .reduce()
+ */
+
+
+
+
+
