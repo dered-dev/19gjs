60001c6488c1b580905c3ba27f620f689ef923ab
dered-dev <jlcc170@gmail.com>
2022-05-27 22:57:54 -0500

    ADD homeork

--- js/homework.js
+++ js/homework.js
@@ -1,127 +1,48 @@
-// Ejercicios Obligatorios
-
 /**
- * Dado el sigueinte arreglo de ciudades
- *   ['méXicO', 'PErÚ', 'eSpAña', 'inGlaterrA']
- * Generar una funcion que capitalize cada elemento del array
- * => Se puede resolver con .forEach() o .map()
+ * Tarea
+ * REalizar los siguientes 3 ejercicios
+ * 
  */
 
-
-/** 
- * Dado un arreglo con nombres de personas, 
- * Funcion que devuele un arreglo con los nombres de las personas que empiezan con vocales y además capitalizados
- * 
- * onlyNamesVowels( ['jorge','ana','ivan','sergio','oscar' ] )
- * -> ['Ana','Ivan','Oscar']
- * 
- * => Se puede resolver con .forEach() o .filter()
-*/
-
-
 /**
- * 
- * Dado una arreglo compuesto por arreglos, 
- * crea una funcion que calcule la suma de los arreglos
- * 
- * additionMultiArr(  [	[1,2,3] , [1,3,2] , [3,2,1] ] )
- * -> 18
+ * 1. funcion con .reduce()
+ * capitalizeNames( ['jorge','lUis', 'marcOs', 'mario'] )
+ * -> ['Jorge','Luis', 'Marcos', 'Mario']
  */
 
-
-const additionMultiArr = (arregloDeArreglos) => {
-
-    let counter = 0
-    arregloDeArreglos.forEach( (arreglo) => {
-        arreglo.forEach( (numero) => {
-            counter += numero
-        })
-    })
-
-    return counter
-}
-
-additionMultiArr(  [	
-        [
-            1,
-            2,
-            3
-        ] , 
-        [1,3,2] , 
-        [3,2,1] 
-    ] )
-
- 
-
+/**
+ * 2. funcion con .reduce()
+ * Dado un array de numeros, filtras solo los pares
+ * filterEvens( [2,3,4,5] )
+ * -> [2,4]
+ */
 
 /**
- * 
- * Estudiar el  metodo reduce
- * https://www.digitalocean.com/community/tutorials/js-finally-understand-reduce
- * https://dataquarium.io/little-bits-reduce/
- * https://www.programiz.com/javascript/library/array/reduce
+ * 3. funcion con .reduce()
+ * Dado un array de numeros, obtener la suma de solo los elementos positivos
+ * addAllPositives( [ 1, -4, 12, 0, -3, 29, -150] )
+ * -> 42
  */
 
 
 /**
- * 
- * Estudiar el tema de Objetos
- * https://www.programiz.com/javascript/object
- * https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Object
- * https://www.freecodecamp.org/espanol/news/javascript-crear-objecto-como-definir-objetos-en-js/
- * https://www.w3schools.com/js/js_objects.asp
- * 
+ * 4. Estudiar Objetos y métodos de objetos
+ *
+ */
+
+
+/**
+ * Opcional
+ * funcion con .reduce()
+ * Dado un array de palabras, filtras las que son palindromos
+ * onlyPalindrome( ['oso','pedro', 'jorge', 'seres'] )
+ * -> ['oso','seres']
  */
 
 
 
-// Ejercicios opcionales
-
-
-/**
- * 
-    En un array con pedidos de un restaurant
-    la estructura por cada pedido es:
-    ['entrada','principal','postre','precio'],
-
-    let pedidos = [
-        ['ensalada de pepinos','paella','platano', 100'],
-        ['ensalada de tomates','pescado','helado', 120],
-        ['ensalada simple','paella','yogurt', 80],
-        ['ensalada simple','enchiladas','yogurt', 80],
-        ['ensalada cesar','salmón','platano', 100],
-    ]
-
-    Crear: 
-
-    Funcion que devuelve el total de pedidos que tienen como plato principal "paella"
-    => Se puede resolver con .forEach() o .filter()
-
-    Funcion que devuelve los pedidos que tienen como postre "platano" o "helado" 
-    => Se puede resolver con .forEach() o .filter()
-
-    Funcion que devuelve los pedidos que tienen un precio superior a 90 
-    => Se puede resolver con .forEach() o .filter()
-    
-*/
-
-let pedidos = [
-    ['ensalada de pepinos','paella','platano', 100],
-    ['ensalada de tomates','pescado','helado', 120],
-    ['ensalada simple','paella','yogurt', 80],
-    ['ensalada simple','enchiladas','yogurt', 80],
-    ['ensalada cesar','salmón','platano', 100],
-]
 
 
 
 
 
-
-let ingredientes = ['chile', 'jitomate', 'cebolla', 'ajo']
-
-const asar = (ingrediente) => {
-    return ingrediente + 'Asado'
-} 
-ingredientes.map( asar ) 
-//-> [chileCocido, jitomateCocido, cebollaCocida, ajoCocido]
