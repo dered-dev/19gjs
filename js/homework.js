79efa84beff1d7a19e95ff41f5c8054b472ad202
dered-dev <jlcc170@gmail.com>
2022-06-01 23:04:40 -0500

    ADD homework

--- js/homework.js
+++ js/homework.js
@@ -1,54 +1,69 @@
 
-
-
-// Ejercicio
 /**
- * Del siguiente objeto
- * Realizar una función que 
- * 1. Calcule el numero de empleados
- * 2. Obtenga el total a pagar
- * employesAndTotal( { 'Albert': 50000, 'jorge': 50000 })
- * -> Son 3 empleados y el total es 100000
+ * Dado un array de objetos koders
+ * Imprimir en consola, todos los nombres de cada koder
+ * 
+ * 'jorge luis Camarillo tiene 30 años y es de la generacion 6'
+ * 'Erik Gutierrez tiene 30 años y es de la generacion 16'
+ * ....
+ * 
  */
 
- let salarios = {
-    'juan': 30000,
-    'Albert': 50000,
-    'jorge': 50000
-}
-
-const employesAndTotal = ( obj ) => {
-
-}
-
+ let koders =  [
+    {
+        name: 'jorge luis',
+        lastName: 'Camarillo',
+        age: 30,
+        generation: 6,
+        modulos: ['js','node js', 'cloud'],
+    },
+    {
+        name: 'Erik',
+        lastName: 'Gutierrez',
+        age: 20,
+        generation: 15,
+        modulos: ['js'],
+    },
+    {
+        name: 'Sara',
+        lastName: 'Reveles',
+        age: 24,
+        generation: 12,
+        modulos: ['js'],
+    }
+]
 
 /**
- * Ejercicio 2.
- * Dado un objeto inicial, hacer los siguientes puntos 
+ * Del mismo array de koders
+ * Obtener la suma de todas las edades
  * 
- * 1. Agregar el lenguaje 'Go' a la lista de lenguajes
- * 2. Cambiar el nivel a 4
- * 3. Eliminar la propiedad avatar
- * 4. Agregar una nueva propiedad de edad y poner el valor de 30
- * 5. Imprimir en consola todos los lenguajes dominados
- * 6. Clonar el objeto en uno nuevo
- * 7. Imprimir en consola el nuevo objeto
- * 
- * @hint https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
  */
 
-let koder = {
-    languages: ["JavaScript", "Python", "Ruby"],
-    isMentor: true,
-    level: 3,
-    avatar: "https://picsum.photos/200/300"
-}
+
 
 
 /**
- * Estudiar los metodos de objetos
- * https://www.w3schools.com/js/js_object_methods.asp
- * https://www.digitalocean.com/community/tutorials/how-to-use-object-methods-in-javascript
- * https://www.programiz.com/javascript/library/object/assign
+ * Del objeto library
+ * 1. Obtener el numero de libros que se estan leyendo
+ * 2. Obtener una lista de todos los autores
+ * 3. Obtener una lista de todos los Libros
  * 
  */
+ let library = [ 
+    {
+        author: 'Bill Gates',
+        title: 'The Road Ahead',
+        readingStatus: true
+    },
+    {
+        author: 'Steve Jobs',
+        title: 'Walter Isaacson',
+        readingStatus: true
+    },
+    {
+        author: 'Suzanne Collins',
+        title:  'Mockingjay: The Final Book of The Hunger Games', 
+        readingStatus: false
+    }];
+
+
