bedc4bf132f557608d660f07cde5c2ace416045c
dered-dev <jlcc170@gmail.com>
2022-06-02 22:49:08 -0500

    ADD homework

---
+++ js/homework.js
@@ -0,0 +1,60 @@
+
+
+
+/**
+ * Ejercicio 1
+ * <ul id="menu" class="menu">
+        <li class="item__menu">Home</li>
+        <li class="item__menu">Products</li>
+        <li class="item__menu">About Us</li>
+    </ul>
+    Replicar con JS este markup
+ */
+
+
+
+
+/**
+ * Dado un arreglo de koders
+ * 1. Generar una lista con la clase koders
+ * 2. Agregar a cada koder en esa lista
+ * 3. A todos los koders agregarles la clase 'item koder'
+ */
+
+ let koders =  [
+    {
+        name: 'jorge luis',
+        lastName: 'Camarillo',
+        age: 30,
+        generation: 6,
+        modulos: ['js','node js', 'cloud'], 
+    },
+    {
+        name: 'Erik',
+        lastName: 'Gutierrez',
+        age: 20,
+        generation: 15,
+        modulos: ['Node'], 
+    },
+    {
+        name: 'Sara',
+        lastName: 'Reveles',
+        age: 24,
+        generation: 12,
+        modulos: ['js'], 
+    }
+]
+
+/**
+ * Opcional
+ * Del mismo arreglo de koders
+ * 1. Generar una tabla de koders con las columnas
+ *   - Nombre completo
+ *   - Edad
+ *   - Generación 
+ * 
+ *   Opcional -Modulos cursados
+ */
+
+
+
