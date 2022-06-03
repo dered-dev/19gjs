27327dc2096461c9d824eb20955b28c1bf9167c7
dered-dev <jlcc170@gmail.com>
2022-06-02 19:26:02 -0500

    ADD DOM

---
+++ js/dom.js
@@ -0,0 +1,26 @@
+
+
+// DOM
+
+
+
+
+//  1. Selecionar por ID
+// document.getElementById('') 
+// document.getElementsByTagName('')
+// document.getElementsByClassName('')
+// document.querySelector('')
+// document.querySelectorAll('')
+
+
+// 2. comprobar atributos
+// element.getAttribute('')
+// element.setAttribute('attr','value')
+// comprobar si existe un atributo
+// document.querySelector('#head__title').hasAttribute('class')
+
+
+
+// document.createElement('ul')
+// ulElement.appendChild(liFirst)w
+// element.textContent = 'test'
