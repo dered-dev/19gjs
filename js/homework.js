492f0f92b8832d6b20c184751bace9342d284972
dered-dev <jlcc170@gmail.com>
2022-06-07 21:07:19 -0500

    ADD homework solution

--- js/homework.js
+++ js/homework.js
@@ -334,10 +334,54 @@ let users = [
 
 
 const filterUsers = ( strToFilter ) => {
-  
+  strToFilter = strToFilter.toLowerCase()
+  let filteredUsers = users.filter( (user) => {
+    if(
+      user.name.toLowerCase().includes(strToFilter) ||
+      user.username.toLowerCase().includes(strToFilter) ||
+      user.email.toLowerCase().includes(strToFilter)
+    ){
+      return user
+    }
+  })
+  return filteredUsers
 }
 
-// filterUsers('brad')
+const printUserList = (usersFiltered) => {
+  let allUserLayout = usersFiltered.reduce((acc, user, idx, arr) => {
+    return acc += `
+    <a href="#" class="list-group-item list-group-item-action " aria-current="true">
+      <div class="d-flex w-100 justify-content-between">
+        <h5 class="mb-1">${user.name}</h5>
+      </div>
+      <p class="mb-1">${user.phone}</p>
+      <small>${user.email}</small>
+    </a>
+    `
+  }, '')
+
+  document.getElementById('users__filtered').innerHTML = allUserLayout
+}
+// cuando cargue la pagina, cargar todos los usuarios
+document.addEventListener('DOMContentLoaded',  printUserList(users))
+
+let inputSearch = document.getElementById('filter__user')
+inputSearch.addEventListener('keyup', () => {
+  // obtener el valor a buscar
+  let searchTerm = inputSearch.value
+  // Filtrar los usuarios
+  let usersFiltrados = filterUsers(searchTerm)
+  // imprimirlos en consola
+  console.log(usersFiltrados)
+  // agregarlos al layout en #lista__users
+  printUserList(usersFiltrados)
+})
+
+
+
+
+
+
 
 
 
