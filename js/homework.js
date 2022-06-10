7c5c0d80ca2f29aecc543411ffeb181905dc5c93
dered-dev <jlcc170@gmail.com>
2022-06-09 22:46:26 -0500

    ADD corrections homework

--- js/homework.js
+++ js/homework.js
@@ -1,15 +1,31 @@
 
 /**
  * 
- * 1. LLenar endpoint de firebase con posts
- * 2. Crear una funcion que obtenga todos los posts
- * y los imprima en pantalla
+ * 1. LLenar el endpoint de firebase con multiples posts
+ * 2. Crear una funcion que obtenga todos los posts y los imprima en una lista de cards
+ * Referencia:
+ * https://getbootstrap.com/docs/5.2/components/card/#content
  * 
- * 3. 
- *  Crear un formulario
- *  3 campos (titulo, contenido, fecha )
- *  1 boton de enviar
- *  cuando se de click en enviar, crear un nuevo post
+ * 3. Crear un nuevo documento html con un formulario que tenga lo siguiente
+ * 3 campos (titulo, contenido, fecha )
+ * 1 botón de enviar
+ * Referecia:
+ * https://getbootstrap.com/docs/5.2/forms/form-control/
+ * 
+ *  Acciones
+ *  cuando se de click en el boton de enviar, 
+ *  crear un nuevo post en firebase
+ *  Opcional: Mostrar un alert con el aviso de que ya se creo el post
+ * 
+ * Referencia: 
+ * https://getbootstrap.com/docs/5.2/components/alerts/#content
+ */
+
+// Estructura sugerida de archivos
+/**
+ * createuser.html
+ * js/createPost.js
+ * css/createpost.css
  */
 
 // Esctructura del post
@@ -22,5 +38,4 @@
 // let title = document.querySelector('#elemento').value
 // let body = document.querySelector('#elemento').value
 // let date = document.querySelector('#elemento').value
-
 
