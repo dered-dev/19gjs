cf6936bb4c1cacda3d015d6d00a57ca0dbe38c05
dered-dev <jlcc170@gmail.com>
2022-06-09 22:38:01 -0500

    ADD homework

---
+++ js/homework.js
@@ -0,0 +1,26 @@
+
+/**
+ * 
+ * 1. LLenar endpoint de firebase con posts
+ * 2. Crear una funcion que obtenga todos los posts
+ * y los imprima en pantalla
+ * 
+ * 3. 
+ *  Crear un formulario
+ *  3 campos (titulo, contenido, fecha )
+ *  1 boton de enviar
+ *  cuando se de click en enviar, crear un nuevo post
+ */
+
+// Esctructura del post
+// { title, body, date }
+
+// url de firebase
+//  * urlFirebase /posts/.json
+
+// como obtener los valores de los input, textarea, select ...
+// let title = document.querySelector('#elemento').value
+// let body = document.querySelector('#elemento').value
+// let date = document.querySelector('#elemento').value
+
+
