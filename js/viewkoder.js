d34099a600cc2b43861149c4a60f00c603031898
dered-dev <jlcc170@gmail.com>
2022-06-13 20:54:34 -0500

    ADD vie koder

--- js/viewkoder.js
+++ js/viewkoder.js
@@ -12,6 +12,30 @@ console.log(idKoder)
 
 // Hacer una peticion al server
 
+// crear conexion
+const xhttp = new XMLHttpRequest()
+xhttp.open( 'GET', `https://koders19gjs-default-rtdb.firebaseio.com/koders/${idKoder}.json`, true)
+xhttp.onload = function(data) {
+    if(data.target.status >= 200 && data.target.status <= 399){
+        let response = JSON.parse(data.target.response)
+        let { name, age, biography, bootcamp } = response
+        let template = `
+                <div class="col-12 col-sm-6">
+                <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">${name} ${age} años</h5>
+                    <p class="card-text">${biography.slice(0, 20)}...</p>
+                    <p class="card-text">${bootcamp}</p>
+                    <a href="/index.html" class="btn btn-link">Ver todos los koders</a>
+                </div>
+                </div>
+            </div>
+        `
+        document.querySelector('.wrap__koder').innerHTML = template
+    }
+}
+xhttp.send()
+
 // cuando el response este listo
 // pintar la informacion del koder en un card
 
