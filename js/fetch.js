3c23ad1cb497f78187a98f9abf61359734e02198
dered-dev <jlcc170@gmail.com>
2022-06-14 23:01:18 -0500

    ADD catch error solution

--- js/fetch.js
+++ js/fetch.js
@@ -23,9 +23,12 @@
 // peticiones GET
 fetch('https://koders19gjs-default-rtdb.firebaseio.com/koders/.json')
 .then( response => {
+    // comprobamos que el estatus de la respuesta es falso
     if (!response.ok) {
+        // si si, lanzamos un error con un mensaje
         throw new Error(`Algo salio mal, status: ${response.status} ${response.statusText} type: ${response.type}`)
     } else {
+        // sino, retornamos la respuesta al siguiente then
         return response.json()
     }
 })
